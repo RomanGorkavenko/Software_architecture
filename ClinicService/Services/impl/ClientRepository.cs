2231fae1d273887131c70cacfbcf1a30f69fa4a2
RomanGorkavenko <roman@gorkavenko.ru>
2023-10-03 17:17:12 +0300

    init commit

--- ClinicService/Services/impl/ClientRepository.cs
+++ ClinicService/Services/impl/ClientRepository.cs
@@ -6,6 +6,7 @@ namespace ClinicService.Services.impl
     public class ClientRepository : IClientRepository
     {
 
+
         private const string connectionString = "Data Source = clinic.db;";
 
 
