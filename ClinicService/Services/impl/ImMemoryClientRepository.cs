f87e4fd5285aa45024267197551738fb4754ae2b
RomanGorkavenko <roman@gorkavenko.ru>
2023-10-03 17:15:24 +0300

    Add project files.

---
+++ ClinicService/Services/impl/ImMemoryClientRepository.cs
@@ -0,0 +1,32 @@
+﻿using ClinicService.Models;
+
+namespace ClinicService.Services.impl
+{
+    public class ImMemoryClientRepository : IClientRepository
+    {
+        int IRepository<Client, int>.Create(Client item)
+        {
+            throw new NotImplementedException();
+        }
+
+        int IRepository<Client, int>.Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Client> IRepository<Client, int>.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        Client IRepository<Client, int>.GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        int IRepository<Client, int>.Update(Client item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
